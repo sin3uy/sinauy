758897874790389e73c087496e61f791a8499224
sin3uy <sin3uy@gmail.com>
2024-09-09 19:05:11 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    if (sidebar.classList.contains('active')) {
-        sidebar.classList.remove('active');
-    } else {
-        sidebar.classList.add('active');
-    }
-}
